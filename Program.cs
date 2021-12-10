0ac84fe2de2205daf0e4f531fcbb1e949617a89f
Magnus Altgård <magnus.altgard@outlook.com>
2021-12-10 17:39:22 +0100

    remove hello world

--- Program.cs
+++ Program.cs
@@ -1,5 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-var day = new Day10();
+﻿var day = new Day10();
 System.Console.WriteLine(day.RunB());
